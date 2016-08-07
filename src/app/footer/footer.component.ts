c5e5abe43dfb821bedb72d6a010d741bafe7edc4
Nadeen Southgate <noddysouthgate@gmail.com>
2016-08-07 14:26:03 +0200

    Added sticky footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'footer',
+  selector: 'kn-footer',
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
     './footer.style.css'
