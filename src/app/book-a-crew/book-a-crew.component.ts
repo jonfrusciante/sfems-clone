533131d9c6302a9392808eaf7d438bfb80f6e2c1
Nadeen Southgate <noddysouthgate@gmail.com>
2016-08-09 21:05:42 +0200

    Started form

--- src/app/book-a-crew/book-a-crew.component.ts
+++ src/app/book-a-crew/book-a-crew.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import {MaterializeDirective} from "angular2-materialize";
 import {Container} from '../container';
+import { REACTIVE_FORM_DIRECTIVES, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'kn-book-a-crew',
   templateUrl: 'book-a-crew.component.html',
-  directives: [MaterializeDirective, Container],
+  directives: [MaterializeDirective, Container, REACTIVE_FORM_DIRECTIVES],
   styles: [``]
 })
 export class BookACrew {
+  registerForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (!this.registerForm.valid) {
+      return;
+    }
+
+    console.log('>>>>>>> ', this.registerForm.value);
+
+  }
 }
