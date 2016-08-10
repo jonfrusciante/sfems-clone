0abf045167060b07542e1d1d42b6903834bf631c
Nadeen Southgate <noddysouthgate@gmail.com>
2016-08-10 08:55:24 +0200

    Updated form

--- src/app/book-a-crew/book-a-crew.component.ts
+++ src/app/book-a-crew/book-a-crew.component.ts
@@ -1,38 +1,39 @@
 import { Component } from '@angular/core';
 import {MaterializeDirective} from "angular2-materialize";
 import {Container} from '../container';
-import { REACTIVE_FORM_DIRECTIVES, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES, ControlGroup, FormBuilder, Validators, AbstractControl} from '@angular/common';
+
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'kn-book-a-crew',
   templateUrl: 'book-a-crew.component.html',
-  directives: [MaterializeDirective, Container, REACTIVE_FORM_DIRECTIVES],
+  directives: [MaterializeDirective, Container, FORM_DIRECTIVES],
   styles: [``]
 })
 export class BookACrew {
-  registerForm: FormGroup;
+  form: ControlGroup;
   people: FirebaseListObservable<any[]>;
 
-  constructor(private formBuilder: FormBuilder, af: AngularFire) {
-    this.people = af.database.list('/people');
-  }
-
-  ngOnInit() {
-    this.registerForm = this.formBuilder.group({
+  constructor(private formBuilder: FormBuilder, private af: AngularFire) {
+    this.form = formBuilder.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required]
     });
   }
 
+  ngOnInit() {
+    this.people = this.af.database.list('/people');    
+  }
+
   onSubmit() {
-    if (!this.registerForm.valid) {
+    if (!this.form.valid) {
       return;
     }
 
-    console.log('>>>>>>> ', this.registerForm.value);
+    console.log('>>>>>>> ', this.form.value);
 
-    this.people.push(this.registerForm.value);
+    this.people.push(this.form.value);
 
   }
 }
